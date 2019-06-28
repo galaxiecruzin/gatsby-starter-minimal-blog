bf796cab35d980721b00df80ede9a6a015c84bfa
Alex Barger <cruzinthegalaxie@gmail.com>
2019-06-28 17:34:22 -0400

    This is a test
    Only a test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,26 @@
-const config = require('./config')
-
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
-
 module.exports = {
-  pathPrefix: config.pathPrefix,
-  siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'post',
-        path: `${__dirname}/blog`,
-      },
+    siteMetadata: {
+        title: `A sample site using gatsby-source-wordpress`,
+        subtitle: `Data fetched from a site hosted on wordpress`
     },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: config.googleAnalyticsID,
-      },
-    },
-    {
-      resolve: 'gatsby-mdx',
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: 'gatsby-remark-external-links',
+    plugins: [
+        {
+            resolve: `gatsby-source-wordpress`,
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
+                baseUrl: `neo1seo.com`,
+                protocol: `https`,
+                hostingWPCOM: false,
+                useACF: false,
             },
-          },
-          {
-            resolve: 'gatsby-remark-images',
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-glamor`,
+        {
+            resolve: `gatsby-plugin-typography`,
             options: {
-              maxWidth: 830,
-              quality: 90,
-              withWebp: true,
-              linkImagesToOriginal: false,
+                pathToConfigModule: `src/utils/typography.js`,
             },
-          },
-          // TODO: Replace with "mdx-component-autolink-headers"
-          {
-            resolve: 'gatsby-remark-autolink-headers',
-            options: {
-              maintainCase: false,
-            },
-          },
-        ],
-      },
-    },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-lodash',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.siteTitleAlt,
-        short_name: config.siteTitleManifest,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'standalone',
-        icon: config.favicon,
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-  ],
+        }
+    ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,94 +1,84 @@
-const _ = require('lodash')
+const path = require(`path`)
+const slash = require(`slash`)
 
-// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
-const wrapper = promise =>
-  promise.then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-    return result
-  })
-
-exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-
-  let slug
-
-  if (node.internal.type === 'Mdx') {
-    if (
-      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
-      Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
-    ) {
-      slug = `/${_.kebabCase(node.frontmatter.slug)}`
-    } else if (
-      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
-      Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
-    ) {
-      slug = `/${_.kebabCase(node.frontmatter.title)}`
-    }
-    createNodeField({ node, name: 'slug', value: slug })
-  }
-}
-
+// Implement the Gatsby API “createPages”. This is
+// called after the Gatsby bootstrap is finished so you have
+// access to any information necessary to programmatically
+// create pages.
+// Will create pages for WordPress pages (route : /{slug})
+// Will create pages for WordPress posts (route : /post/{slug})
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const postTemplate = require.resolve('./src/templates/post.js')
-  const categoryTemplate = require.resolve('./src/templates/category.js')
-
-  const result = await wrapper(
-    graphql(`
-      {
-        allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-          nodes {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              categories
-            }
+  // The “graphql” function allows us to run arbitrary
+  // queries against the local Gatsby GraphQL schema. Think of
+  // it like the site has a built-in database constructed
+  // from the fetched data that you can run queries against.
+  const result = await graphql(`
+    {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            slug
+            status
+            template
           }
         }
       }
-    `)
-  )
+      allWordpressPost {
+        edges {
+          node {
+            id
+            slug
+            status
+            template
+            format
+          }
+        }
+      }
+    }
+  `)
 
-  const posts = result.data.allMdx.nodes
+  // Check for any errors
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
 
-  posts.forEach((n, index) => {
-    const next = index === 0 ? null : posts[index - 1]
-    const prev = index === posts.length - 1 ? null : posts[index + 1]
+  // Access query results via object destructuring
+  const { allWordpressPage, allWordpressPost } = result.data
 
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+  // We want to create a detailed page for each
+  // page node. We'll just use the WordPress Slug for the slug.
+  // The Page ID is prefixed with 'PAGE_'
+  allWordpressPage.edges.forEach(edge => {
+    // Gatsby uses Redux to manage its internal state.
+    // Plugins and sites can use functions like "createPage"
+    // to interact with Gatsby.
     createPage({
-      path: n.fields.slug,
-      component: postTemplate,
+      // Each page is required to have a `path` as well
+      // as a template component. The `context` is
+      // optional but is often necessary so the template
+      // can query data specific to each page.
+      path: `/${edge.node.slug}/`,
+      component: slash(pageTemplate),
       context: {
-        slug: n.fields.slug,
-        prev,
-        next,
+        id: edge.node.id,
       },
     })
   })
 
-  const categorySet = new Set()
-
-  _.each(posts, n => {
-    if (_.get(n, 'frontmatter.categories')) {
-      n.frontmatter.categories.forEach(cat => {
-        categorySet.add(cat)
-      })
-    }
-  })
-
-  const categories = Array.from(categorySet)
-
-  categories.forEach(category => {
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  // We want to create a detailed page for each
+  // post node. We'll just use the WordPress Slug for the slug.
+  // The Post ID is prefixed with 'POST_'
+  allWordpressPost.edges.forEach(edge => {
     createPage({
-      path: `/categories/${_.kebabCase(category)}`,
-      component: categoryTemplate,
+      path: `/${edge.node.slug}/`,
+      component: slash(postTemplate),
       context: {
-        category,
+        id: edge.node.id,
       },
     })
   })
